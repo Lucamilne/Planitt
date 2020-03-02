c57961ccfd5e27fcc42c0a36d414177797dfd30b
Lucamilne <lucamilne@googlemail.com>
2020-03-02 16:18:42 +0000

    add nav bar

--- planitt.js
+++ planitt.js
@@ -1,3 +1,21 @@
+const rollingText = document.querySelector(".rolling-text p");
+
 $("#landing-animation").delay(2000).slideUp(600);
 
 $("main").delay(3000).fadeIn();
+
+
+rollingText.addEventListener("animationend", function() {
+    document.getElementById("article-1").scrollIntoView();
+    $(".rolling-text p").fadeOut();
+})
+
+$(".branded-menu").click(function() {
+    $("nav").toggleClass("toggle-nav")
+    invertHeader();
+})
+
+function invertHeader() {
+    $(".branded-menu").toggleClass("invert")
+    $("header button").toggleClass("invert-button")
+}
