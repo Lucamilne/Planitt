aace6ca46ef2c7e21fc707c4c326630edd648a76
Luca Milne <lucamilne@googlemail.com>
2020-03-04 21:22:38 +0000

    cleaned up html class and ID names, removed superfluous content

--- planitt.js
+++ planitt.js
@@ -1,13 +1,18 @@
 const rollingText = document.querySelector(".rolling-text p");
+const slide = document.querySelector(".slide");
+const body = document.body;
 
 $("#landing-animation").delay(2000).slideUp(600);
 
 $("main").delay(3000).fadeIn();
 
+function scrollTo(hash) {
+    document.getElementById(`${hash}`).scrollIntoView();
+}
 
-// rollingText.addEventListener("animationend", function() {
-//     document.getElementById("article-1").scrollIntoView();
-// })
+rollingText.addEventListener("animationend", function() {
+    document.getElementById("product-styles").scrollIntoView();
+})
 
 $(".branded-menu").click(function() {
     $("nav").toggleClass("toggle-nav")
@@ -23,3 +28,38 @@ function invertHeader() {
 function toggleMenuIcon() {
     $(".branded-menu i").toggleClass("fa-bars fa-times")
 }
+
+
+//kitchen-selector :hover label highlighting
+$(".column").hover(function() {
+    $(this).children().css({
+        "background-color": "rgba(255,255,255,0.5",
+        "color": "#222",
+        "transform": "translate(10px, 0)"
+    })
+}, function() {
+    $(this).children().css({
+        "background-color": "rgba(0, 0, 0, 0.3)",
+        "color": "#eee",
+        "transform": "translate(0,0)"
+    })
+})
+
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+$(window).scroll(function () {
+    $('.step').each(function () {
+        if (isScrolledIntoView(this) === true) {
+            $(this).addClass('slideAnimation');
+        }
+    });
+
+});
